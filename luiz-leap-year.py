2d34c878b42ece2568f4f8dcfa1081dc2db80798
Luiz CVBSJ <luizcvbsj@gmail.com>
2023-12-08 23:39:48 +0100

    created a more performatic way to calculate luiz-leap-years.py

--- luiz-leap-year.py
+++ luiz-leap-year.py
@@ -1,7 +1,5 @@
 import time
 
-# Record the start time
-start_time = time.time()
 
 
 print("Welcome to the program that tells you if an year is a leap year!")
@@ -12,20 +10,33 @@ print("")
 years_to_calculate = int(input("Number of years: ?"))
 count = 0
 
-for year in range(years_to_calculate+1):
-    if year % 400 == 0:
-        print(f"{year} is a leap year!")
-        count += 1
-    elif year % 100 == 0:
-        print(f"{year} is not a leap year.")
-    elif year % 4 == 0:
-        print(f"{year} is a leap year!")
-        count += 1
-    else:
-        print(f"{year} is not a leap year")
+mode = int(input("Mode 1 or 2?"))
 
-print(f"{count} leap years counted in the space of {years_to_calculate} years.")
+# Record the start time
+start_time = time.time()
+if mode == 1:
+    for year in range(years_to_calculate+1):
+        if year % 400 == 0:
+            print(f"{year} is a leap year!")
+            count += 1
+        elif year % 100 == 0:
+            print(f"{year} is not a leap year.")
+        elif year % 4 == 0:
+            print(f"{year} is a leap year!")
+            count += 1
+        else:
+            print(f"{year} is not a leap year")
 
+    print(f"{count} leap years counted in the space of {years_to_calculate} years.")
+elif mode == 2:
+    for year in range(years_to_calculate+1):
+        if year % 400 == 0 or year % 100 != 0 and year % 4 == 0:
+            print(f"{year} is a leap year!")
+            count += 1
+
+    print(f"{count} leap years counted in the space of {years_to_calculate} years.")
+else:
+    print("Mode not recognized!")
 
 # Record the end time
 end_time = time.time()
