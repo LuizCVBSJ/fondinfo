2ffb12949f51dfc63b2425a94c97e9657e090b56
Luiz CVBSJ <luizcvbsj@gmail.com>
2024-01-07 11:08:29 +0100

    finished factorial tests

--- luiz-factorial.py
+++ luiz-factorial.py
@@ -1,2 +1,13 @@
-def factorial(n: int):
-    return (n * factorial(n-1))
+def factorial(n: int) -> int:
+    result = 1
+    print(f"n is {n}")
+    if n > 0:
+        print(f"Starting factorial of {n-1}")
+        print(f"Multiplying {n} by {n-1}")
+        result = n * factorial(n-1)
+    print(f"returning {result}")
+    return result
+
+number = int(input("Factorial of Number: \n"))
+
+print(f"The factorial of the number {number} is {factorial(number)}")
