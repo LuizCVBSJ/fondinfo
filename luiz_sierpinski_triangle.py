c7a460b1e6fcb9d991ef3d1b5a471be762babf3c
Luiz CVBSJ <luizcvbsj@gmail.com>
2024-01-13 17:00:22 +0100

    added luiz_sierpinski.py to test recursive function on sierpinski triangle

---
+++ luiz_sierpinski_triangle.py
@@ -0,0 +1,11 @@
+import g2d_pyodide as g2d
+
+w, h = 600, 600
+
+def sierpinski_triangle(x, y, w, h):
+    w2, h2 = w // 2, h // 2
+    if
+
+def main():
+    g2d.init_canvas((w, h))
+    g2d.set_color((0, 0, 0))
