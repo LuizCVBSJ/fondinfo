480a66330731066fe37444971800f5deee737e20
Luiz CVBSJ <luizcvbsj@gmail.com>
2024-01-23 21:37:21 +0100

    added luiz_bisection.py to use recursion to execute a bisection function

---
+++ luiz_bisection.py
@@ -0,0 +1,18 @@
+def f(x: float) -> float:
+    return x**3 - x - 1
+
+def find_zero(a: float, b: float):
+    half = (a + b) / 2
+    f_half = f(half)
+
+    if abs(f_half) < 0.001:
+        return half
+    elif f_half * f(a) < 0:
+        return find_zero(a, half)  
+    else:
+        return find_zero(half, b)
+
+def main():
+    print(find_zero(1, 2))
+
+main()
