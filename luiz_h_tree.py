e31f6ad5ff8fc58ff2909fcde97c35682ce07cb0
Luiz CVBSJ <luizcvbsj@gmail.com>
2024-01-23 20:31:27 +0100

    polished luiz_h_tree.py to have a level setting of how many hs to draw.

--- luiz_h_tree.py
+++ luiz_h_tree.py
@@ -1,22 +1,23 @@
 import g2d_pyodide as g2d 
 
-def h_tree(w0, h0, w, h):
-    if w < 20 or h < 20:
+def h_tree(w0, h0, w, h, level):
+    if w < 10 or h < 10 or level == 0:
         return 
 
     g2d.draw_line((w0+w/4, h0+h/4), (w0+w/4, h0+h/4*3,))
     g2d.draw_line((w0+w/4*3, h0+h/4), (w0+w/4*3, h0+h/4*3))
     g2d.draw_line((w0+w/4, h0+h/2), (w0+w/4*3, h0+h/2))
 
-    h_tree(w0, h0, w/2, h/2)
-    h_tree(w0+w/2, h0, w/2, h/2)
-    h_tree(w0, h0+h/2, w/2, h/2)
-    h_tree(w0+w/2, h0+h/2, w/2, h/2)
+    h_tree(w0, h0, w/2, h/2, level-1)
+    h_tree(w0+w/2, h0, w/2, h/2, level-1)
+    h_tree(w0, h0+h/2, w/2, h/2, level-1)
+    h_tree(w0+w/2, h0+h/2, w/2, h/2, level-1)
 
 def main():
 
     w, h = 600, 600
     g2d.init_canvas((w, h))
-    h_tree(0, 0, w, h)
+    level = 5
+    h_tree(0, 0, w, h, level)
 
 main()
