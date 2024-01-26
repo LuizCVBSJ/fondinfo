ee85523cad7ce70a7d4afc36bb9e85b801d843db
Luiz CVBSJ <luizcvbsj@gmail.com>
2024-01-26 10:35:45 +0100

    properly finished luiz_sierpinski_triangle.py to use recursion to make the triangles and used a potency of 2 to get rid of the remnant lines

--- luiz_sierpinski_triangle.py
+++ luiz_sierpinski_triangle.py
@@ -1,11 +1,24 @@
 import g2d_pyodide as g2d
 
-w, h = 600, 600
-
-def sierpinski_triangle(x, y, w, h):
-    w2, h2 = w // 2, h // 2
-    if
-
 def main():
+    w, h = 512, 512
     g2d.init_canvas((w, h))
     g2d.set_color((0, 0, 0))
+    g2d.draw_rect((0, 0), (w, h))
+    sierpinski(0, 0, w, h, 5)
+
+def sierpinski(x, y, w, h, level):
+    if w < 5 or level == 0:
+        return
+
+    w2 = w/2
+    h2 = h/2
+
+    g2d.set_color((255, 255, 255))
+    g2d.draw_rect((x + w2, y), (w2, h2))
+
+    sierpinski(x, y, w2, h2, level - 1)
+    sierpinski(x, y + h2, w2, h2, level - 1)
+    sierpinski(x + w2, y + h2, w2, h2, level - 1)
+
+main()
